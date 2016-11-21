7f4a23ea2952298981a541b4d94e109b875927ae
Kenta Kuroda <tk.777.k@gmail.com>
2016-11-21 20:31:34 +0900

    Change comments

--- app/src/main/java/com/cain_96/othello/Fragment/MainFragment.java
+++ app/src/main/java/com/cain_96/othello/Fragment/MainFragment.java
@@ -1,7 +1,7 @@
 package com.cain_96.othello.Fragment;
 
 /**
- * Created by kuro on 2016/11/21.
+ * Created by Cain96 on 2016/11/21.
  */
 public class MainFragment {
 }
--- app/src/main/java/com/cain_96/othello/Helper/MainProcess.java
+++ app/src/main/java/com/cain_96/othello/Helper/MainProcess.java
@@ -1,7 +1,7 @@
 package com.cain_96.othello.Helper;
 
 /**
- * Created by kuro on 2016/11/21.
+ * Created by Cain96 on 2016/11/21.
  */
 public class MainProcess {
 }
