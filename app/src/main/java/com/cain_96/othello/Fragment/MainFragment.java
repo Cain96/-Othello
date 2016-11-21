8b909ba6d9a52f6a47ed7cb4ce67ceb8452d1d48
Kenta Kuroda <tk.777.k@gmail.com>
2016-11-21 20:15:45 +0900

    Create Main Fragment and Process

---
+++ app/src/main/java/com/cain_96/othello/Fragment/MainFragment.java
@@ -0,0 +1,7 @@
+package com.cain_96.othello.Fragment;
+
+/**
+ * Created by kuro on 2016/11/21.
+ */
+public class MainFragment {
+}
---
+++ app/src/main/java/com/cain_96/othello/Helper/MainProcess.java
@@ -0,0 +1,7 @@
+package com.cain_96.othello.Helper;
+
+/**
+ * Created by kuro on 2016/11/21.
+ */
+public class MainProcess {
+}
